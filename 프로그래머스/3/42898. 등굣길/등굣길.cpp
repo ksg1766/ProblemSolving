97bc4f7b025f6c4d8ba665520cb7d610accc6075
SeongGyu Kim <ksg7186@gmail.com>
2024-05-20 17:27:17 +0900

    [level 3] Title: 등굣길, Time: 0.05 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -6,26 +6,30 @@ using namespace std;
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
     
-    vector<vector<int>> cost(n + 1, vector<int>(m + 1, 1));
-    
-    for(int i = 0; i <= n; ++i)
-    {
-        cost[i][0] = -1;
-        cost[i][1] = 1;
-    }
-    
-    for(int i = 0; i <= m; ++i)
-    {
-        cost[0][i] = -1;
-        cost[1][i] = 1;
-    }
+    vector<vector<int>> cost(n + 1, vector<int>(m + 1, 0));
     
     for(auto& iter : puddles)
         cost[iter[1]][iter[0]] = -1;
     
+    for(int i = 1; i <= n; ++i)
+    {
+        if(-1 == cost[i][1])
+            break;
+        
+        cost[i][1] = 1;
+    }
+    
     for(int i = 1; i <= m; ++i)
     {
-        for(int j = 1; j <= n; ++j)
+        if(-1 == cost[1][i])
+            break;
+        
+        cost[1][i] = 1;
+    }
+    
+    for(int i = 2; i <= m; ++i)
+    {
+        for(int j = 2; j <= n; ++j)
         {    
             if(-1 == cost[j][i])
                 continue;
